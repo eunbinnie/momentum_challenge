0efd924f7bbcb1c4469f2d2cb8e7b964d6c9c848
eunbinnie <dmsqlsdl54@naver.com>
2023-01-27 12:35:36 +0900

    resize body background color

--- js/day2.js
+++ js/day2.js
@@ -1,10 +1,33 @@
-const h2 = document.querySelector("h2");
-const width = window.innerWidth;
+const body = document.body;
 
-if (width > 1200) {
-  h2.classList.add("computer");
-} else if (width > 400) {
-  h2.classList.add("pad");
-} else {
-  h2.classList.add("phone");
+const COMPUTER = "computer";
+const PAD = "pad";
+const PHONE = "phone";
+
+function addClassList(name) {
+  body.classList.add(name);
 }
+
+function removeClassList(name1, name2) {
+  body.classList.remove(name1, name2);
+}
+
+function resizeEvent() {
+  const size = window.innerWidth;
+
+  if (size > 1000) {
+    addClassList(COMPUTER);
+    removeClassList(PHONE, PAD);
+  }
+  else if (size > 600) {
+    addClassList(PAD);
+    removeClassList(PHONE, COMPUTER);
+  }
+  else {
+    addClassList(PHONE);
+    removeClassList(PAD, COMPUTER);
+  }
+}
+
+resizeEvent();
+window.addEventListener('resize', resizeEvent);
