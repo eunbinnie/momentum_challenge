9ce8a069e39f9340d38687449bac4322a641f2d8
eunbinnie <dmsqlsdl54@naver.com>
2023-02-01 19:38:52 +0900

    디데이 계산기 프로그램 완성

--- js/time.js
+++ js/time.js
@@ -5,10 +5,10 @@ function printDate() {
   const today = new Date();
   const dDay = christmas - today;
 
-  const date = Math.floor(String(dDay / (1000 * 60 * 60 * 24)).padStart(3, "00"));
-  const hours = Math.floor(String((dDay / (1000 * 60 * 60)) % 24).padStart(2, "0"));
-  const minutes = Math.floor(String((dDay / (1000 * 60)) % 60).padStart(2, "0"));
-  const seconds = Math.floor(String(dDay / 1000 % 60).padStart(2, "0"));
+  const date = String(Math.floor(dDay / (1000 * 60 * 60 * 24))).padStart(3, "00");
+  const hours = String(Math.floor((dDay / (1000 * 60 * 60)) % 24)).padStart(2, "0");
+  const minutes = String(Math.floor((dDay / (1000 * 60)) % 60)).padStart(2, "0");
+  const seconds = String(Math.floor(dDay / 1000 % 60)).padStart(2, "0");
 
   h2.innerText = `${date}d ${hours}h ${minutes}m ${seconds}s`;
 }
